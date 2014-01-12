9b009bb81d85de435842e3553e5c4b4ddb9257ff
mike <mdebreceni@gmail.com>
2014-01-12 13:22:16 -0700

    Initial import

---
+++ RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/ButtonHandler.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package xyzzy.courseracourse.assignment1.rockpaperscissorslizardspock;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ * @author mike
+ *
+ */
+public class ButtonHandler {
+
+
+	public void pressRock () 
+	{
+		
+	}
+
+	public void pressPaper ()
+	{
+	}
+
+	public void pressScissors () 
+	{
+	}
+	public void pressLizard () 
+	{
+
+	}
+	public void pressSpock () {
+
+	}
+
+}
---
+++ RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/RpslsMoves.java
@@ -0,0 +1,9 @@
+package xyzzy.courseracourse.assignment1.rockpaperscissorslizardspock;
+
+public interface RpslsMoves {
+	public static final int MOVE_ROCK = 0;
+	public static final int MOVE_SPOCK = 1;
+	public static final int MOVE_PAPER = 2;
+	public static final int MOVE_LIZARD = 3;
+	public static final int MOVE_SCISSORS = 4;
+};
