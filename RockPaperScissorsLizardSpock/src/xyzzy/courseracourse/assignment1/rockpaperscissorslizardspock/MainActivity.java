0caf133fe9217cec96cca1ead373d1f7b8eac625
Mike Debreceni <mdebreceni@gmail.com>
2014-01-12 20:49:00 -0500

    Implement game logic (but it's a little buggy still)

--- RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/MainActivity.java
+++ RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/MainActivity.java
@@ -1,11 +1,17 @@
 package xyzzy.courseracourse.assignment1.rockpaperscissorslizardspock;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.content.res.*;
 
 public class MainActivity extends Activity {
-
+    rpsls rpslsGame;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +24,77 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	public void playRock(View view) {
+		Log.d("rpsls", "playRock: view height is " + view.getHeight());
+		
+		Log.d("rpsls", "playRock()");
+		
+		String s;
+		s = playRound(rpsls.MOVE_ROCK, makeComputerMove());
+		Log.d("rpsls", "playRock() returning.  s = " + s);
+	
+	}
+
+	public void playPaper(View view) {
+		
+		showResult(playRound(rpsls.MOVE_PAPER, makeComputerMove()));
+
+	}
+
+	public void playScissors(View view) {
+		showResult(playRound(rpsls.MOVE_SCISSORS, makeComputerMove()));
+
+	}
+	
+	public void playLizard(View view) {
+		showResult(playRound(rpsls.MOVE_LIZARD, makeComputerMove()));
+	}
+
+	public void playSpock(View view) {
+		showResult(playRound(rpsls.MOVE_SPOCK, makeComputerMove()));
+	}
+
+	public int makeComputerMove()
+	{
+		Log.d("rpsls", "makeComputerMove");
+		Random rand = new Random();
+		int move = rand.nextInt(5);
+		Log.d("rpsls", "makeComputerMove() returning");
+		return move;
+	}
+
+	public void showResult(String gameOutcome) {
+		// display a popup containing game outcome in string form
+		Log.d("rpsls", gameOutcome);
+	}
+
+	public String playRound(int playerMove, int computerMove) 
+	{
+		Resources res = getResources();
+		
+		String outcome = "TODO";
+		// play a game of rock paper scissors lizard spock 
+		int difference = (computerMove - playerMove) % 5;
+		Log.d("rpsls","playRound("+playerMove+","+computerMove+")");
+		outcome = res.getString(R.string.you_played) + res.getStringArray(R.array.moves)[playerMove];
+		outcome += res.getString(R.string.and_i_played) + res.getStringArray(R.array.moves)[computerMove];
+		if (difference == 0) {
+			outcome += res.getString(R.string.tie);
+			
+			// tie
+		} else if(difference > 2) {
+			outcome += res.getString(R.string.you_win);
+			// player wins
+		} else {
+			outcome += res.getString(R.string.i_win);
+			// computer wins
+	
+		}
+
+		Log.d("rpsls", outcome);
+		return outcome;
+	}
+
 
 }
--- RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/rpsls.java
+++ RockPaperScissorsLizardSpock/src/xyzzy/courseracourse/assignment1/rockpaperscissorslizardspock/rpsls.java
@@ -1,58 +1,5 @@
 package xyzzy.courseracourse.assignment1.rockpaperscissorslizardspock;
-import java.util.Random;
 
 class rpsls implements RpslsMoves {
 
-	public int makeComputerMove()
-	{
-		Random rand = new Random();
-		int move = rand.nextInt(5);
-		return move;
-	}
-
-	public void showResult(String gameOutcome) {
-		// display a popup containing game outcome in string form
-
-	}
-
-	public void playRock() {
-		showResult(playRound(MOVE_ROCK, makeComputerMove()));
-	}
-
-	public void playPaper() {
-		showResult(playRound(MOVE_PAPER, makeComputerMove()));
-
-	}
-
-	public void playScissors() {
-		showResult(playRound(MOVE_PAPER, makeComputerMove()));
-
-	}
-
-	public void playLizard() {
-		showResult(playRound(MOVE_LIZARD, makeComputerMove()));
-
-	}
-
-	public void playSpock() {
-		showResult(playRound(MOVE_SPOCK, makeComputerMove()));
-	}
-
-	public String playRound(int playerMove, int computerMove) 
-	{
-		String outcome = "TODO";
-		// play a game of rock paper scissors lizard spock 
-		int difference = (computerMove - playerMove) % 5;
-		
-		if (difference == 0) {
-			// tie
-		} else if(difference > 2) {
-			// player wins
-		} else {
-			// computer wins
-
-		}
-
-		return outcome;
-	}
 }
